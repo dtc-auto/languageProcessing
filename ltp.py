fe71dbe7a5c0e8a263cdeaeddd1e7c83e9dc9b05
Wang Mengjue <wmjdhr@sina.com>
2018-05-14 17:05:55 +0800

    update demo

--- ltp.py
+++ ltp.py
@@ -1,5 +1,14 @@
 import pymssql
+import pandas as pd
 
+server = "SQLDEV02\sql"
+server = "127.0.0.1"
+user = "dtc"
+password = "asdf1234"
+database = "Autohome_WOM"
 
+sql = '''select top 50 manipulation from src.Raw_Data'''
+conn = pymssql.connect(server, user, password, database)
+df = pd.read_sql_query(sql, conn)
 
-pymssql.connect()
+print([item.manipulation for item in df.itertuples()])
