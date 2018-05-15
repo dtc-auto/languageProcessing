7caff1bc8b3e5dbec1102fc573e1067373f8afae
Zhang Mingqiu <dtc_auto@outlook.com>
2018-05-15 17:41:47 +0800

    Added definitions

--- ltp.py
+++ ltp.py
@@ -1,15 +1,39 @@
 import pymssql
 import pandas as pd
+import os
 
+LTP_DATA_DIR = 'C:\Users\Zhang Mingqiu\PycharmProjects\languageProcessing\ltp_data_v3.4.0'  # ltp模型目录的路径
+cws_model_path = os.path.join(LTP_DATA_DIR, 'cws.model')
 server = "SQLDEV02\sql"
 server = "127.0.0.1"
 user = "dtc"
 password = "asdf1234"
 database = "Autohome_WOM"
-
-sql = '''select top 50 manipulation from src.Raw_Data'''
 conn = pymssql.connect(server, user, password, database)
-df = pd.read_sql_query(sql, conn)
+sql = '''select top 50 * from dw.Comments_Unpivot'''
 
-print([item.manipulation for item in df.itertuples()])
-print()
+
+def read_the_sql(sql_sentence):
+    df = pd.read_sql_query(sql_sentence, conn)
+    return df
+
+read = read_the_sql(sql)
+print([item.value for item in read.itertuples()])
+
+#pyltp suedo code
+#分句
+#for item in df.itertuples()
+#sentencessplitter.split()
+
+
+def split_into_sentences():
+    sents = SentenceSplitter().split(sentence.value for sentence in read.itertuples())
+    return sents
+
+sentences = split_into_sentences()
+
+
+def split_into_words():
+    segmentor = Segmentor()
+    segmentor.load(cws_model_path)
+    words = segmentor.segment(for sentence in sentences)
